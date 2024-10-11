0bb15ef5beb2fe2a49a85246d16d9848477bcaeb
DTristan <tristan.diard@ynov.com>
2024-10-11 10:35:59 +0200

    First Commit

--- bs_client_I1.py
+++ bs_client_I1.py
@@ -9,11 +9,19 @@ port = 13337               # Port choisir par le serveur
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connexion au serveur
-s.connect((host, port))
+
+try :
+    s.connect((host, port))
+except socket.error as msg:
+    print(f"Erreur de connexion avec le serveur :")
+    sys.exit(1)
+
 # note : la double parenthèse n'est pas une erreur : on envoie un tuple à la fonction connect()
 
 # Envoi de data bidon
 s.sendall(b'Meowwwww')
+
+
 
 # On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
 data = s.recv(1024)
