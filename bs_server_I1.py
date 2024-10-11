eb4b3dcfa4cdee4cd8042159a8988f58823801a7
DTristan <tristan.diard@ynov.com>
2024-10-11 10:21:38 +0200

    First Commit

--- bs_server_I1.py
+++ bs_server_I1.py
@@ -16,7 +16,7 @@ s.listen(1)
 conn, addr = s.accept()
 # Dès que quelqu'un se connecte, on affiche un message qui contient son adresse
 print('Connected by', addr)
-
+message = "Hi mate !"
 # Petite boucle infinie (bah oui c'est un serveur)
 # A chaque itération la boucle reçoit des données et les traite
 while True:
@@ -32,7 +32,7 @@ while True:
         print(f"Données reçues du client : {data}")
 
         # On répond au client un truc
-        conn.sendall('Hi mate !')
+        conn.sendall(bytes(message))
 
     except socket.error:
         print("Error Occured.")
