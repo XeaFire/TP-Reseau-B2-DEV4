f345867259e9a5dce46d2c69d07c7571337b2542
DTristan <tristan.diard@ynov.com>
2024-10-17 11:48:23 +0200

    gpasletemps

--- Part2/utils/logs.py
+++ Part2/utils/logs.py
@@ -6,7 +6,7 @@ from os import mkdir, path, access, W_OK
 
 
 TEMP_DIR = "/tmp"
-LOG_DIR = f"{TEMP_DIR}/network_tp3"
+LOG_DIR = f"{TEMP_DIR}/network_tp4"
 LOG_FILE = f"{LOG_DIR}/network.log"
 
 
@@ -26,20 +26,14 @@ def create_log_dir() -> bool:
     return True
 
 
-class LogLevel(Enum):
-    """Simple log level enums"""
 
-    INFO = 1
-    ERROR = 2
-
-
-def log(msg: str, log_level: LogLevel) -> True:
+def log(msg: str, log_level:str) -> True:
     """Writes given msg to LOG_FILE adding a timestamp"""
     # on récupère un timestamp au bon format pour la ligne de log
     log_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     # construction de la ligne de log
-    log_line = f"{log_timestamp} [{log_level.name}] {msg}\n"
+    log_line = f"{log_timestamp} [{log_level}] {msg}\n"
 
     with open(LOG_FILE, "a", encoding="utf-8") as f:
         f.write(log_line)
