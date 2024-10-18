14875aa34a7a990ffc881116ad92f4b8282431de
DTristan <tristan.diard@ynov.com>
2024-10-18 11:50:45 +0200

    gpasletemps

--- Part2/install.sh
+++ Part2/install.sh
@@ -2,7 +2,6 @@
 sudo mkdir -p /var/log/bs_server
 sudo touch /var/log/bs_server/bs_server.log
 sudo chown $(whoami):$(whoami) /var/log/bs_server/bs_server.log
-
-sudo mkdir -p /var/log/bs_server
+sudo mkdir -p /var/log/bs_client
 sudo touch /var/log/bs_client/bs_client.log
 sudo chown $(whoami):$(whoami) /var/log/bs_client/bs_client.log
