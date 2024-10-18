cd2e7fa21c9204d5da39090772d088cd648a5e71
DTristan <tristan.diard@ynov.com>
2024-10-18 12:40:46 +0200

    gpasletemps

--- Part3/bs_client_III.py
+++ Part3/bs_client_III.py
@@ -37,7 +37,7 @@ utils.logs.log(f"Réponse reçue du serveur {host} : {data}", "INFO", False, "/v
 s.close()
 
 # Affichage de la réponse reçue du serveur
-print(int.from_bytes(data), "big")
+print(int.from_bytes(data, byteorder='big'))
 
 sys.exit(0)
 
